e3ffa83eab3df549961e140bbcd402e0e3c58fd7
René Vögeli <rvoegeli@rangee.com>
2016-05-23 14:00:26 +0200

    Add lockdown mode, where the user is only able to edit a layout provided by command line parameter, apply and save it.

--- screenlayout/gui.py
+++ screenlayout/gui.py
@@ -106,10 +106,44 @@ class Application(object):
         </toolbar>
     </ui>
     """
+    
+    uixml_lockdown = """
+    <ui>
+        <menubar name="MenuBar">
+            <menu action="Layout">
+                <menuitem action="Save" />
+                <separator />
+                <menuitem action="Apply" />
+                <separator />
+                <menuitem action="Quit" />
+            </menu>
+            <menu action="View">
+                <menuitem action="Zoom4" />
+                <menuitem action="Zoom8" />
+                <menuitem action="Zoom16" />
+            </menu>
+            <menu action="Outputs" name="Outputs">
+                <menuitem action="OutputsDummy" />
+            </menu>
+            <menu action="Help">
+                <menuitem action="About" />
+            </menu>
+        </menubar>
+        <toolbar name="ToolBar">
+            <toolitem action="Save" />
+            <toolitem action="Apply" />
+            <separator />
+            <toolitem action="Quit" />
+        </toolbar>
+    </ui>
+    """
 
-    def __init__(self, file=None, randr_display=None, force_version=False):
+    def __init__(self, file=None, randr_display=None, force_version=False, lockdown=False):
         self.window = window = gtk.Window()
         window.props.title = "Screen Layout Editor"
+        
+        self.lockdown = lockdown
+        self.file = file
 
         # actions
         actiongroup = gtk.ActionGroup('default')
@@ -117,6 +151,7 @@ class Application(object):
             ("Layout", None, _("_Layout")),
             ("New", gtk.STOCK_NEW, None, None, None, self.do_new),
             ("Open", gtk.STOCK_OPEN, None, None, None, self.do_open),
+            ("Save", gtk.STOCK_SAVE, None, None, None, self.do_save),
             ("SaveAs", gtk.STOCK_SAVE_AS, None, None, None, self.do_save_as),
 
             ("Apply", gtk.STOCK_APPLY, None, '<Control>Return', None, self.do_apply),
@@ -151,7 +186,10 @@ class Application(object):
 
         self.uimanager.insert_action_group(actiongroup, 0)
 
-        self.uimanager.add_ui_from_string(self.uixml)
+        if self.lockdown:
+            self.uimanager.add_ui_from_string(self.uixml_lockdown)
+        else:
+            self.uimanager.add_ui_from_string(self.uixml)
 
         # widget
         self.widget = widget.ARandRWidget(display=randr_display, force_version=force_version)
@@ -235,6 +273,15 @@ class Application(object):
             self.filetemplate = self.widget.load_from_file(f)
 
     @actioncallback
+    def do_save(self):
+        try:
+            self.file
+        except NameError:
+            pass
+        else:
+            self.widget.save_to_file(self.file, self.filetemplate)
+
+    @actioncallback
     def do_save_as(self):
         d = self._new_file_dialog(_("Save Layout"), gtk.FILE_CHOOSER_ACTION_SAVE, gtk.STOCK_SAVE)
         d.props.do_overwrite_confirmation = True
@@ -304,6 +351,7 @@ def main():
     p = optparse.OptionParser(usage="%prog [savedfile]", description="Another XRandrR GUI", version="%%prog %s"%__version__)
     p.add_option('--randr-display', help='Use D as display for xrandr (but still show the GUI on the display from the environment; e.g. `localhost:10.0`)', metavar='D')
     p.add_option('--force-version', help='Even run with untested XRandR versions', action='store_true')
+    p.add_option('--lockdown', help='Lockdown', action='store_true')
 
     (options, args) = p.parse_args()
     if len(args) == 0:
@@ -316,6 +364,7 @@ def main():
     a = Application(
             file=file_to_open,
             randr_display=options.randr_display,
-            force_version=options.force_version
+            force_version=options.force_version,
+            lockdown=options.lockdown
             )
     a.run()
